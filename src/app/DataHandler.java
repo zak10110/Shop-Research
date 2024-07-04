8af7cf9ac07da6106efb464f975d77a505f77f8d
zak10110 <zr380678644997@gmail.com>
2024-07-04 17:57:17 +0300

    Revised DataHandler class, adapted to handle data type T

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,11 +1,14 @@
 package app;
 
 public class DataHandler {
-    public String handleData(Object[] items) {
+    // Узагальнений метод, який має параметр як масив узагальненого типу
+    // та повертає String значення обробленого масиву
+    public <T> String handleData(T[] items) {
         StringBuilder sb = new StringBuilder();
         int count = 0;
+
         // Цикл обробки масиву узагальненого типу
-        for (Object item : items) {
+        for (T item : items) {
             count++;
             sb.append(String.format("(%d) %s ", count, item));
         }
